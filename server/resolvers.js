54ee41fdacd79e8bdae77e9222e3306564012bc4
Sebastiannnnnn <sebastian.nikkonen@accenture.com>
2018-06-11 23:59:16 +0300

    update resolver logic with promises

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,8 @@
 import ChatMessage from './mongoose/chatmessage'
 import mongoose from 'mongoose';
 import { host, login } from './constants';
+import 'babel-core/register';
+import 'babel-polyfill';
 
 "use strict";
 
@@ -16,49 +18,75 @@ db.once('open', () => {console.log( 'Connected to MongoDB')})
 const resolvers = {
     Query: {
         allMessages: () => {
-            return ChatMessage.find();
+            return ChatMessage.find()
+            .then((res) => {
+                return res;
+            })
         },
         messagesByUser: (root, {user}) => {
-            return ChatMessage.find({user: user});
+            return ChatMessage.find({user: user})
+            .then((res) => {
+                return res;
+            })
         },
         messageById: (root, {id}) => {
-            return ChatMessage.find({id: id});
+            return ChatMessage.find({id: id})
+            .then((res) => {
+                return res;
+            })
         },
         messagesLength: () => {
-            return ChatMessage.count({}, function(err, results) {
-                return results;
+            return ChatMessage.count({})
+            .then((res) => {
+                return res
             });
         }
     },
     Mutation: {
-        addMessage: (root, {input}) => {
+        addMessage: async (root, {input}) => {
             let time = new Date().getTime();
             input.id = input.user + '-' + time;
-            input.timestamp = new Date().getTime();
+            input.timestamp = time;
 
             var message = new ChatMessage(input);
 
-            message.save(function (err, message) {
-                if (err) console.error(err);
-            });
+            let resultSet = null;
+            await message.save(message)
+                .then((res) => {
+                    resultSet = res;
+                }).catch((err) => {
+                    resultSet = new Error('Error happened');
+                });
 
-            return input;
+            return resultSet;
         },
-        updateMessage: (root, {input}) => {
+        updateMessage: async (root, {input}) => {
             input.timestamp = new Date().getTime();
+            let conditions = { id: input.id };
+            let update = {$set: {message : input.message} }
+            let options = { upsert: true, new: true };
 
-            ChatMessage.findOneAndUpdate({ id: input.id, user: input.user }, {$set: {message : input.message, timestamp : input.timestamp} }, function(err, doc){
-                if(err) console.log(err);
-            });
+            let resultSet;
+            await ChatMessage.findOneAndUpdate(conditions, update, options)
+                .then((res) => {
+                    resultSet = res;
+                }).catch((err) => {
+                    resultSet = new Error('Error happened');
+                });
 
-            return input;
+            return resultSet;
         },
-        deleteMessage: (root, {id}) => {
-            ChatMessage.findOneAndRemove({id: id}, function(err, message){
-                if(err) console.log(err);
-            });
+        deleteMessage: async (root, {id}) => {
+            let conditions = {id: id};
+            let resultSet;
+            await ChatMessage.findOneAndRemove(conditions)
+                .then((res) => {
+                    resultSet = res;
+                }).catch((err) => {
+                    resultSet = new Error('Error happened');
+                });
 
-            return id;
+            return resultSet;
         }
     }
 }
